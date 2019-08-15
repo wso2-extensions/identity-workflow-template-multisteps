4efa40402d9239f9b5db0f1581c314757264ecfa
Nipuni Paaris <se01nipuni@gmail.com>
2019-08-15 22:46:58 +0530

    Change logs to final

--- components/org.wso2.carbon.identity.workflow.template/src/main/java/org/wso2/carbon/identity/workflow/template/internal/WorkflowTemplateServiceComponent.java
+++ components/org.wso2.carbon.identity.workflow.template/src/main/java/org/wso2/carbon/identity/workflow/template/internal/WorkflowTemplateServiceComponent.java
@@ -42,7 +42,7 @@ import org.osgi.service.component.annotations.ReferencePolicy;
          immediate = true)
 public class WorkflowTemplateServiceComponent {
 
-    private static Log log = LogFactory.getLog(WorkflowTemplateServiceComponent.class);
+    private static final Log log = LogFactory.getLog(WorkflowTemplateServiceComponent.class);
 
     @Activate
     protected void activate(ComponentContext context) {
